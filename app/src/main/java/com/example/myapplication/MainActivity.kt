0a0b10741050c6f34050e2033f9b7593f61e1b22
DanielEOS <daniel.ayala@inpsercom.com>
2026-06-02 16:37:10 -0500

    Commit calculadora IMC APP lógica pantalla

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -23,3 +23,37 @@ class MainActivity : ComponentActivity() {
         }
     }
 }
+
+@Composable
+fun CalculadoraIMCApp() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "inicio"
+    ) {
+        composable("inicio") {
+            PantallaIngreso(navController)
+        }
+
+        composable(
+            route = "resultado/{nombre}/{imc}",
+            arguments = listOf(
+                navArgument("nombre") { type = NavType.StringType },
+                navArgument("imc") { type = NavType.FloatType }
+            )
+        ) { backStackEntry ->
+            val nombre = backStackEntry.arguments?.getString("nombre") ?: ""
+            val imc = backStackEntry.arguments?.getFloat("imc") ?: 0f
+
+            PantallaResultado(
+                nombre = nombre,
+                imc = imc,
+                onVolver = {
+                    navController.popBackStack()
+                }
+            )
+        }
+    }
+}
+
